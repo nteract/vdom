2f784e6da2fa786f2113936ba244a745290bcf1c
Kyle Kelley <rgbkrk@gmail.com>
2017-09-30 11:04:15 -0700

    run yapf over the test file

--- vdom/tests/test_core.py
+++ vdom/tests/test_core.py
@@ -6,17 +6,17 @@ from ..core import _flatten_children, toJSON
 
 def test_flatten_children():
     # when the first argument is an array, interpret it as the primary argument
-    assert _flatten_children([1,2,3]) == [1,2,3]
+    assert _flatten_children([1, 2, 3]) == [1, 2, 3]
     # otherwise positional arguments are fine
-    assert _flatten_children(1,2,3) == [1,2,3]
+    assert _flatten_children(1, 2, 3) == [1, 2, 3]
     # keyword argument of children is ok too
-    assert _flatten_children(children=[1,2,3]) == [1,2,3]
+    assert _flatten_children(children=[1, 2, 3]) == [1, 2, 3]
 
     # precedence tests
     # kwargs[children] is highest priority
-    assert _flatten_children([1],2, children=[3]) == [3]
+    assert _flatten_children([1], 2, children=[3]) == [3]
     # array as first argument second highest priority
-    assert _flatten_children([1],2) == [1]
+    assert _flatten_children([1], 2) == [1]
     # otherwise, positional arguments are the default
     assert _flatten_children(2) == [2]
 
