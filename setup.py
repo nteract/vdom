648ef7edba88eb00eb20c5c0a9676d1c7f8d2d54
Kyle Kelley <rgbkrk@gmail.com>
2017-10-28 15:53:03 -0700

    include schema in package data, load relative

--- setup.py
+++ setup.py
@@ -3,7 +3,6 @@
 
 #!/usr/bin/env python
 
-
 from os.path import exists
 from setuptools import setup
 import versioneer
@@ -14,28 +13,23 @@ import sys
 if sys.version_info[0] < 3 and 'bdist_wheel' not in sys.argv:
     ipython_req = 'ipython<6'
 
-extras_require = {
-    "tests": ["pytest"]
-}
+extras_require = {"tests": ["pytest"]}
 
-extras_require['all']= list(
-    set([val for k, v in extras_require.items() for val in v])
-    )
+extras_require['all'] = list(
+    set([val for k, v in extras_require.items() for val in v]))
 
-setup(name='vdom',
-      version=versioneer.get_version(),
-      cmdclass=versioneer.get_cmdclass(),
-      description='VDOM for Python',
-      author='nteract contributors',
-      author_email='jupyter@googlegroups.com',
-      license='BSD',
-      keywords="vdom, html",
-      long_description=(open('README.md').read() if exists('README.md') else ''),
-      url='https://github.com/nteract/vdom',
-      packages=['vdom'],
-      install_requires=[
-          ipython_req,
-          'jsonschema'
-      ],
-      extras_require=extras_require
-     )
+setup(
+    name='vdom',
+    version=versioneer.get_version(),
+    cmdclass=versioneer.get_cmdclass(),
+    description='VDOM for Python',
+    author='nteract contributors',
+    author_email='jupyter@googlegroups.com',
+    license='BSD',
+    keywords="vdom, html",
+    long_description=(open('README.md').read() if exists('README.md') else ''),
+    url='https://github.com/nteract/vdom',
+    packages=['vdom'],
+    package_data={'vdom': ['schemas/vdom_schema_v0.json']},
+    install_requires=[ipython_req, 'jsonschema'],
+    extras_require=extras_require)
