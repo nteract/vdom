759f91e7419b3881b40154097d6eda1df6d32715
yuvipanda <yuvipanda@gmail.com>
2018-04-30 17:12:19 -0700

    Refactor vdom module to be smaller
    The code is now shorter and (arguably) more 'pythonic'.
    
    Major structural changes:
    
    1. VDOM objects are no longer containers for unstructured
       dicts. They have a fixed set of attributes (tag_name,
       children, key and attributes). This is enforced
       with __slots__.
    2. A new Component class is not created when calling
       `create_component`. Instead, a partial of the
       VDOM constructor is created.
    3. The JSON Schema is only checked when a VDOM object is
       being deserialized from JSON.
    
    Major behavior changes:
    
    1. Children are no longer 'flattened'. Node children will always
       be a list of 0 or more elements. This is compliant with the
       spec. Tests have been modified to accomodate this.
    2. Do not support passing arbitrary schemas to VDOM constructor.
       VDOM is a structured object now, so this is unnecessary.

--- vdom/tests/test_core.py
+++ vdom/tests/test_core.py
@@ -16,32 +16,6 @@ _vdom_schema_file_path = os.path.join(
 with io.open(_vdom_schema_file_path, "r") as f:
     VDOM_SCHEMA = json.load(f)
 
-def test_flatten_children():
-    # when the first argument is an array, interpret it as the primary argument
-    assert _flatten_children([1, 2, 3]) == [1, 2, 3]
-    # otherwise positional arguments are fine
-    assert _flatten_children(1, 2, 3) == [1, 2, 3]
-    # keyword argument of children is ok too
-    assert _flatten_children(children=[1, 2, 3]) == [1, 2, 3]
-
-    # precedence tests
-    # kwargs[children] is highest priority
-    assert _flatten_children([1], 2, children=[3]) == [3]
-    # array as first argument second highest priority
-    assert _flatten_children([1], 2) == [1]
-    # otherwise, positional arguments are the default
-    assert _flatten_children(2, 3) == [2, 3]
-
-    # an empty array returns an empty array
-    assert _flatten_children([]) == []
-    # with no arguments, we get a null
-    assert _flatten_children() == None
-
-    # If the first argument is None, we assume they explicitly wanted a
-    # null element for a VDOMEl (this is ok)
-    assert _flatten_children(None) == None
-    assert _flatten_children(None, 1, None) == [None, 1, None]
-
 
 def test_to_json():
     assert to_json({
@@ -60,11 +34,11 @@ def test_to_json():
 
     assert to_json(div(h1('Our Incredibly Declarative Example'))) == {
         'tagName': 'div',
-        'children': {
+        'children': [{
             'tagName': 'h1',
-            'children': 'Our Incredibly Declarative Example',
+            'children': ['Our Incredibly Declarative Example'],
             'attributes': {}
-        },
+        }],
         'attributes': {}
     }
 
@@ -79,7 +53,7 @@ def test_to_json():
         'div',
         'children': [{
             'tagName': 'h1',
-            'children': 'Our Incredibly Declarative Example',
+            'children': ['Our Incredibly Declarative Example'],
             'attributes': {}
         }, {
             'tagName':
@@ -88,14 +62,13 @@ def test_to_json():
             'children': [
                 'Can you believe we wrote this ', {
                     'tagName': 'b',
-                    'children': 'in Python',
+                    'children': ['in Python'],
                     'attributes': {}
                 }, '?'
             ]
         }, {
             'tagName': 'img',
-            'children':
-            None,
+            'children': [],
             'attributes': {
                 'src':
                 'https://media.giphy.com/media/xUPGcguWZHRC2HyBRS/giphy.gif'
@@ -113,9 +86,6 @@ def test_schema_validation():
     with pytest.raises(ValidationError):
         test_vdom = VDOM([_valid_vdom_obj], )
 
-    # make sure you can pass empty schema
-    assert (VDOM([_valid_vdom_obj],
-                 schema={}).json_contents == [_valid_vdom_obj])
   
     # check that you can pass a valid schema
     assert (VDOM(_valid_vdom_obj, schema=VDOM_SCHEMA))
